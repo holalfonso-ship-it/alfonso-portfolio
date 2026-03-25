0afb68faff66fba02060000e679c37c6acfaf4ca
Alfonso Zamorano <holalfonso@gmail.com>
2026-03-25 10:58:34 +0100

    Update testimonial content for Franc Andreu
    Franc testimonial

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,6 +4,6 @@ const testimonials = [
         author: "Franc Andreu",
         position: "Product Design Team Lead",
         company: "Leadtech Group",
-        content: "I had the opportunity to work with Alfonso for two years at Leadtech Group. His expertise in Design Ops is unparalleled, and his proactive approach consistently drove successful outcomes for our projects."
+        content: "He's an experienced professional who identifies and resolves complex usability issues on the various projects he works on. I would also like to highlight his expertise in Design Ops, which has contributed to the team's growth. He always approaches his work proactively and is eager to help and serve as a role model for his colleagues. I would recommend Alfonso to any digital product team looking to set a higher standard in every respect."
     }
 ];
