0dbb0194b7be47bf66f5aa9040a1bc9606ced185
Alfonso Zamorano <holalfonso@gmail.com>
2026-03-25 11:10:47 +0100

    Update Testimonials.tsx to add missing export default

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -7,3 +7,5 @@ const testimonials = [
         content: "He's an experienced professional who identifies and resolves complex usability issues on the various projects he works on. I would also like to highlight his expertise in Design Ops, which has contributed to the team's growth. He always approaches his work proactively and is eager to help and serve as a role model for his colleagues. I would recommend Alfonso to any digital product team looking to set a higher standard in every respect."
     }
 ];
+
+export default testimonials;
