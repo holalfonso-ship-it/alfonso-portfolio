ec44cc952c7aeee4fe05734f499677f94f7e6fb3
Alfonso Zamorano <holalfonso@gmail.com>
2026-03-26 14:11:06 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Hero from '@/components/Hero';
-import FeaturedCaseStudy from '@/components/FeaturedCaseStudy';
+import CaseStudyTabs from '@/components/CaseStudyTabs';
 import About from '@/components/About';
 import Testimonials from '@/components/Testimonials';
 //import WorkProcess from '@/components/WorkProcess';
@@ -9,40 +9,37 @@ import TransitionEffect from '@/components/TransitionEffect';
 import PersonalProjects from '@/components/PersonalProjects';
 
 const Index: React.FC = () => {
-  useEffect(() => {
-    const handleAnchorClick = (e: MouseEvent) => {
-      const target = e.target as HTMLElement;
-      const anchor = target.closest('a');
-      
-      if (anchor && anchor.hash && anchor.hash.startsWith('#')) {
-        e.preventDefault();
-        const targetElement = document.querySelector(anchor.hash);
-        
-        if (targetElement) {
-          targetElement.scrollIntoView({ behavior: 'smooth' });
-          window.history.pushState(null, '', anchor.hash);
-        }
-      }
-    };
-    
-    document.addEventListener('click', handleAnchorClick);
-    return () => document.removeEventListener('click', handleAnchorClick);
-  }, []);
-  
-  return (
-    <div className="flex flex-col min-h-screen">
-      <TransitionEffect />
-      <main className="flex-grow">
-        <Hero />
-        <FeaturedCaseStudy />
-        <PersonalProjects />
-        <About />
-        <Testimonials />
-       {/* <WorkProcess /> */}
-        <Contact />
-      </main>
-    </div>
-  );
+    useEffect(() => {
+          const handleAnchorClick = (e: MouseEvent) => {
+                  const target = e.target as HTMLElement;
+                  const anchor = target.closest('a');
+                  if (anchor && anchor.hash && anchor.hash.startsWith('#')) {
+                            e.preventDefault();
+                            const targetElement = document.querySelector(anchor.hash);
+                            if (targetElement) {
+                                        targetElement.scrollIntoView({ behavior: 'smooth' });
+                                        window.history.pushState(null, '', anchor.hash);
+                            }
+                  }
+          };
+          document.addEventListener('click', handleAnchorClick);
+          return () => document.removeEventListener('click', handleAnchorClick);
+    }, []);
+
+    return (
+          <div className="flex flex-col min-h-screen">
+                <TransitionEffect />
+                <main className="flex-grow">
+                        <Hero />
+                        <CaseStudyTabs />
+                        <PersonalProjects />
+                        <About />
+                        <Testimonials />
+                  {/* <WorkProcess /> */}
+                        <Contact />
+                </main>main>
+          </div>div>
+        );
 };
 
 export default Index;
